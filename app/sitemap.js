fdc050e09574b317970963fa07c570307f87d41e
ecareerdesigner-lang <ecareerdesigner@gmail.com>
2026-09-23 14:53:50 -0400

    Add pricing page to sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -36,6 +36,12 @@ export default async function sitemap() {
       priority: 0.9,
     },
     {
+      url: `${baseUrl}/pricing`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
+    {
       url: `${baseUrl}/performance-review`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
